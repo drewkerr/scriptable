d7cd15903efdb17717ee4eb1446f3f823971e1b1
Andrew Kerr <drewkerr@users.noreply.github.com>
2020-10-23 21:36:32 +1100

    Update COVID-19 Cases.js
    Added 14 day case average and more compact widget.

--- COVID-19 Cases.js
+++ COVID-19 Cases.js
@@ -66,9 +66,16 @@ async function saveData() {
     range: 'Victorian LGAs!A:D'
   })
 
+  const weeks = url + params({
+    cache: true,
+    sheet: sid,
+    range: 'Vic 14 day average!A:E'
+  })
+
   let casesData = await getData(cases, 10)
   let stateData = await getData(state, 28*8+1)
   let localData = await getData(local, 80)
+  let weeksData = await getData(weeks, 2)
 
   let vicData = stateData.filter(data => data["State/territory"] == "VIC")
   let locData = localData.filter(data => data["LGA"] == "GREATER BENDIGO")
@@ -86,6 +93,8 @@ async function saveData() {
   let data = {
     "stats": {
       "Growth factor": growth.toFixed(2),
+      "Regional 14 day avg": weeksData[0]["Regional Average"],
+      "Metro 14 day average": weeksData[0]["Metro Average"],
       "Local active cases": locData[0]["Active Cases"],
       "Local total cases": locData[0]["Total Cases"],
       "Victoria new cases": vicData[0]["New cases"],
@@ -95,6 +104,12 @@ async function saveData() {
       "Australia active cases": casesData[0]["Current"],
       "Australia total cases": casesData[0]["Confirmed (total)"],
       "Australia total deaths": casesData[0]["Deceased"]
+    },
+    "widget": {
+      "14d": `${weeksData[0]["Metro Average"]} (M) ${weeksData[0]["Regional Average"]} (R)`,
+      "BGO": `${locData[0]["Total Cases"]} (${locData[0]["Active Cases"]} active)`,
+      "VIC": `${vicData[0]["Cumulative confirmed"]} (+${vicData[0]["New cases"]})`,
+      "AUS": `${casesData[0]["Confirmed (total)"]} (+${Object.values(graph)[0].toString()})`
     },
     "graph": month,
     "date": casesData[0]["Date"],
@@ -132,7 +147,10 @@ function columnGraph(data, width, height, colour) {
 
 function createWidget(data) {
   let widget = new ListWidget()
-  widget.setPadding(16, 16, 16, 8)
+  let now = new Date()
+  now.setHours(now.getHours() + 12)
+  widget.refreshAfterDate = now
+//   widget.setPadding(16, 16, 16, 16)
   
   function gradient(start, end) {
     let startColor = new Color(start)
@@ -147,32 +165,40 @@ function createWidget(data) {
   if (growth < 1) gradient("37c25a", "1cb943")
   else gradient("ee676c", "eb5056")
 
-  let head = `COVID-19 growth ${data["stats"]["Growth factor"]}`
-  let headText = widget.addText(head)
+  let header = widget.addStack()
+  let headText = header.addText("COVID-19")
   headText.textColor = Color.white()
-  headText.textOpacity = 0.8
-  headText.textSize = 10
+  headText.font = Font.boldSystemFont(10)
   headText.minimumScaleFactor = 0.5
+  header.addSpacer()
+  let dateText = header.addText(data["date"])
+  dateText.textColor = Color.white()
+  dateText.textOpacity = 0.8
+  dateText.font = Font.regularSystemFont(10)
+  dateText.minimumScaleFactor = 0.5
 
-  let image = columnGraph(data["graph"], 400, 50, Color.white()).getImage()
-  widget.addImage(image)
+  let image = columnGraph(data["graph"], 400, 100, Color.white()).getImage()
+  widget.addImage(image).applyFillingContentMode()
+  let growText = widget.addText(`growth ${data["stats"]["Growth factor"]}`)
+  growText.rightAlignText()
+  growText.textColor = Color.white()
+  growText.textOpacity = 0.8
+  growText.font = Font.regularSystemFont(10)
+  growText.minimumScaleFactor = 0.5
 
   widget.addSpacer()
 
-  let body = {
-    "BENDIGO": `   ${data["stats"]["Local total cases"]} (${data["stats"]["Local active cases"]} active)`,
-    "VICTORIA": `   ${data["stats"]["Victoria total cases"]} (+${data["stats"]["Victoria new cases"]})`,
-    "AUSTRALIA": `   ${data["stats"]["Australia total cases"]} (+${data["stats"]["Australia new cases"]})`
-  }
-  Object.entries(body).forEach(([key, value]) => {
-    let labelText = widget.addText(key)
+  Object.entries(data["widget"]).forEach(([key, value]) => {
+    let stack = widget.addStack()
+    stack.spacing = 5
+    let labelText = stack.addText(key)
     labelText.textColor = Color.white()
     labelText.textOpacity = 0.6
-    labelText.textSize = 10
+    labelText.font = Font.regularSystemFont(12)
     labelText.minimumScaleFactor = 0.5
-    let bodyText = widget.addText(value)
+    let bodyText = stack.addText(value)
     bodyText.textColor = Color.white()
-    bodyText.textSize = 14
+    bodyText.font = Font.regularSystemFont(12)
     bodyText.minimumScaleFactor = 0.5
   })
 
