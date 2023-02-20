8f222a60e3f136d666baf48e23369f262ec0ec43
Andrew Kerr <drewkerr@users.noreply.github.com>
2023-02-20 20:51:30 +1100

    Update COVID-19 Cases.js to use covidlive.com.au

--- COVID-19 Cases.js
+++ COVID-19 Cases.js
@@ -3,28 +3,37 @@
 // icon-color: deep-green; icon-glyph: procedures;
 // Configuration
 const location = "VIC" // ACT, NSW, NT, QLD, SA, TAS, VIC, WA
-
-// Using data sources from these articles:
-// https://www.abc.net.au/news/2020-03-17/coronavirus-cases-data-reveals-how-covid-19-spreads-in-australia/12060704
-// https://www.abc.net.au/news/2021-03-02/charting-australias-covid-vaccine-rollout/13197518
+const url = "https://covidlive.com.au/covid-live.csv"
 
 function parseCSV(str, len = 0) {
-  let arr = [];
-  let quote = false;
-  let col, c;
+  let arr = []
+  let quote = false
   for (let row = col = c = 0; c < str.length && (!!len === row < len); c++) {
-    let cc = str[c], nc = str[c+1];
-    arr[row] = arr[row] || [];
-    arr[row][col] = arr[row][col] || '';
+    let cc = str[c], nc = str[c+1]
+    arr[row] = arr[row] || []
+    arr[row][col] = arr[row][col] || ''
     if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
     if (cc == '"') { quote = !quote; continue; }
     if (cc == ',' && !quote) { ++col; continue; }
     if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
     if (cc == '\n' && !quote) { ++row; col = 0; continue; }
     if (cc == '\r' && !quote) { ++row; col = 0; continue; }
-    arr[row][col] += cc;
+    arr[row][col] += cc
   }
-  return arr;
+  return arr
+}
+
+function filterDay(csv, day = 0) {
+  let latest = csv[1][0].split("-")
+  let daynow = new Date(latest[0], latest[1] - 1, latest[2]).getDay()
+  let offset = (daynow + 7 - day) % 7
+  let filter = [csv[0]]
+  for (let i = offset * 9 + 1; i < csv.length; i = i + 7 * 9) {
+    for (let j = 0; j < 9; j++) {
+      filter.push(csv[i + j])
+    }
+  }
+  return filter
 }
 
 function arrayHash(arr) {
@@ -39,121 +48,56 @@ function arrayHash(arr) {
   })
 }
 
-async function getData(url, len = 0) {
+async function getData(url) {
   let req = new Request(url)
   let txt = await req.loadString()
-  let csv = await parseCSV(txt, len)
-  return await arrayHash(csv)
+  let csv = await parseCSV(txt, 26 * 7 * 9 + 1)
+  let fri = await filterDay(csv, 5)
+  return await arrayHash(fri)
 }
 
-// Source: https://docs.google.com/spreadsheets/d/1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk/edit
-
-const url = 'https://covid-sheets-mirror.web.app/api?'
-const sid = '1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk'
-
-function params(obj) {
-  return Object.entries(obj)
-    .map(([key, val]) => `${key}=${encodeURI(val)}`).join("&")
-}
-
-async function checkData() {
-
-  const meta = url + params({
-    cache: true,
-    sheet: sid,
-    range: 'Metadata!A:B'
-  })
-
-  let data = await getData(meta, 3)
-  if (data[0].Value == "TRUE") {
-    return data[1].Value
-  } else {
-    return false
-  }
-}
-
-async function saveData(updated) {
-
-  const state = url + params({
-    cache: true,
-    sheet: sid,
-    range: 'Active States Daily Count!A:E'
-  })
-
-  const hospt = url + params({
-    cache: true,
-    sheet: sid,
-    range: 'Fed Hospitalisation!A1:G9'
-  })
-  
-  const death = url + params({
-    cache: true,
-    sheet: sid,
-    range: 'Deaths!A:C'
-  })
-  
-  const vaccs = 'https://www.abc.net.au/dat/news/interactives/covid19-data/aus-doses-breakdown.csv'
-
-  let stateData = await getData(state)
-  let hosptData = await getData(hospt)
-  let deathData = await getData(death)
-  let vaccsData = await getData(vaccs)
-  vaccsData = vaccsData.filter(data => data["place"] == "AUS").pop()
+function saveData(data) {
   
   const numstr = s => parseInt(s).toLocaleString()
-  const to2dp = s => parseFloat(s).toFixed(2)
 
-  let myStateData = stateData.filter(data => data["State/territory"] == location)
-  let newDeaths = deathData.filter(data => data["Date"] == deathData[0].Date)
-  let newLocalDeaths = newDeaths.filter(data => data["State/territory"] == location)
-
-  let graph = stateData.reduce((a, b) => {
-    let date = b["Date announced"]
-    let cases = parseInt(b["New cases"]) || 0
-    if (!a[date]) a[date] = cases
-    else a[date] += cases
-    return a
-  }, {})
-  let total = stateData.slice(0,8).reduce((a, b) => {
-    return a + (parseInt(b["Cumulative confirmed"]) || 0)
-  }, 0)
-  const month = Object.values(graph)
-  const t = 7 // 7 day smoothing period
+  const state = data.filter(d => d["CODE"] == location)
+  const national = data.filter(d => d["CODE"] == "AUS")
+  console.log(state)
+  
+  const natcases = national.map(a => parseInt(a["NEW_CASE_CNT"]) || 0)
+  const t = 4 // 4 week smoothing period
   const sum = (a, b) => a + b
-  const tsum = (n) => month.slice(n * t, t + n * t).reduce(sum)
+  const tsum = (n) => natcases.slice(n * t, t + n * t).reduce(sum)
   const growth = Math.pow(tsum(0) / tsum(1), 1 / t)
-  let data = {
-    "stats": {
+
+  data = {
+    stats: {
       "Growth factor": growth.toFixed(2),
-      "New vaccine doses": numstr(vaccsData["daily"]),
-      "Cumulative doses": numstr(vaccsData["total"]),
-      "First Dose 5+": to2dp(vaccsData["totalFirstPct"])+'%',
-      "Second Dose 5+": to2dp(vaccsData["totalSecondPct"])+'%',
-      "Third Dose 18+": to2dp(vaccsData["totalThirdPct_18_plus"])+'%',
-      [location+" new cases"]: numstr(myStateData[0]["New cases"]),
-      [location+" total cases"]: numstr(myStateData[0]["Cumulative confirmed"]),
-      [location+" hospitalised"]: numstr(hosptData.filter(data => data["State/territory"] == location)[0]["Hospitalised "]),
-      [location+" ICU"]: numstr(hosptData.filter(data => data["State/territory"] == location)[0]["ICU"]),
-      [location+" ventilated"]: numstr(hosptData.filter(data => data["State/territory"] == location)[0]["Ventilated"]),
-      [location+" new deaths"]: numstr(newLocalDeaths.length),
-      "Australia new cases": numstr(month[0]),
-      "Australia hospitalised": numstr(hosptData.reduce((a, b) => a + (parseInt(b["Hospitalised "]) || 0), 0)),
-      "Australia ICU": numstr(hosptData.reduce((a, b) => a + (parseInt(b["ICU"]) || 0), 0)),
-      "Australia ventilated": numstr(hosptData.reduce((a, b) => a + (parseInt(b["Ventilated"]) || 0), 0)),
-      "Australia total cases": numstr(total),
-      "Australia new deaths": numstr(newDeaths.length),
-      "Australia total deaths": numstr(deathData[0]["Death ID"])
+      [location+" new cases"]: numstr(state[0]["NEW_CASE_CNT"]),
+      [location+" total cases"]: numstr(state[0]["CASE_CNT"]),
+      [location+" hospitalised"]: numstr(state[0]["MED_HOSP_CNT"]),
+      [location+" ICU"]: numstr(state[0]["MED_ICU_CNT"]),
+      [location+" ventilated"]: numstr(state[0]["MED_VENT_CNT"]),
+      [location+" new deaths"]: numstr(state[0]["DEATH_CNT"] - state[0]["PREV_DEATH_CNT"]),
+      [location+" total deaths"]: numstr(state[0]["DEATH_CNT"]),
+      "Australia new cases": numstr(national[0]["NEW_CASE_CNT"]),
+      "Australia total cases": numstr(national[0]["CASE_CNT"]),
+      "Australia hospitalised": numstr(national[0]["MED_HOSP_CNT"]),
+      "Australia ICU": numstr(national[0]["MED_ICU_CNT"]),
+      "Australia new deaths": numstr(national[0]["DEATH_CNT"] - national[0]["PREV_DEATH_CNT"]),
+      "Australia total deaths": numstr(national[0]["DEATH_CNT"]),
+      "First vaccine doses": numstr(national[0]["VACC_FIRST_DOSE_CNT"]),
+      "Second vaccine doses": numstr(national[0]["VACC_PEOPLE_CNT"]),
+      "Booster vaccine doses": numstr(national[0]["VACC_BOOSTER_CNT"]),
     },
-    "widget": {
-      "ICU": `${hosptData.reduce((a, b) => a + (parseInt(b["ICU"]) || 0), 0)} / ${hosptData.reduce((a, b) => a + (parseInt(b["Hospitalised "]) || 0), 0)}`,
-      [location]: `+${myStateData[0]["New cases"]} -${numstr(newLocalDeaths.length)}`,
-      "AUS": `+${month[0].toString()}  -${numstr(newDeaths.length)}`
+    widget: {
+      "ICU": `${numstr(national[0]["MED_ICU_CNT"])} / ${numstr(national[0]["MED_HOSP_CNT"])}`,
+      [location]: `+${numstr(state[0]["NEW_CASE_CNT"])} -${numstr(state[0]["DEATH_CNT"] - state[0]["PREV_DEATH_CNT"])}`,
+      "AUS": `+${numstr(national[0]["NEW_CASE_CNT"])} -${numstr(national[0]["DEATH_CNT"] - national[0]["PREV_DEATH_CNT"])}`
     },
-    "national": month.slice(0,28),
-    "state": myStateData.map(a => parseInt(a["New cases"]) || 0).slice(0,28),
-    "date": stateData[0]["Date announced"],
-    "vacpc": [to2dp(vaccsData["totalFirstPct"]), to2dp(vaccsData["totalSecondPct"]), to2dp(vaccsData["totalThirdPct_18_plus"])],
-    "updated": updated
+    national: natcases,
+    state: state.map(a => parseInt(a["NEW_CASE_CNT"]) || 0),
+    date: data[0]["REPORT_DATE"]
   }
   let fm = FileManager.iCloud()
   let path = fm.joinPath(fm.documentsDirectory(), "covid19.json")
@@ -192,19 +136,6 @@ function caseGraph(totdata, subdata, width, height, colour) {
   return context
 }
 
-function vaccGraph(firstpc, secondpc, thirdpc, width, height) {
-  console.log(firstpc, secondpc, thirdpc)
-  let context = new DrawContext()
-  context.opaque = false
-  context.size = new Size(width, height)
-  for (const pc of [100, firstpc, secondpc, thirdpc]) {
-    context.setFillColor(new Color("ffffff", 0.2))
-    let rect = new Rect(0, 0, width * pc / 100, height)
-    context.fillRect(rect)
-  }
-  return context
-}
-
 function createWidget(data) {
   let widget = new ListWidget()
   widget.setPadding(16, 16, 16, 16)
@@ -218,8 +149,7 @@ function createWidget(data) {
     widget.backgroundGradient = gradient
   }
 
-  let growth = data["growth"]
-  if (growth < 1) gradient("37c25a", "1cb943")
+  if (data.stats["Growth factor"] < 1) gradient("37c25a", "1cb943")
   else gradient("ee676c", "eb5056")
 
   let header = widget.addStack()
@@ -228,15 +158,15 @@ function createWidget(data) {
   headText.font = Font.boldSystemFont(10)
   headText.minimumScaleFactor = 0.5
   header.addSpacer()
-  let dateText = header.addText(data["date"])
+  let dateText = header.addText(data.date)
   dateText.textColor = Color.white()
   dateText.textOpacity = 0.8
   dateText.font = Font.regularSystemFont(10)
   dateText.minimumScaleFactor = 0.5
 
-  let col = caseGraph(data["national"], data["state"], 400, 100, "ffffff").getImage()
+  let col = caseGraph(data.national, data.state, 200, 100, "ffffff").getImage()
   widget.addImage(col).applyFillingContentMode()
-  let growText = widget.addText(`growth ${data["stats"]["Growth factor"]}`)
+  let growText = widget.addText(`growth ${data.stats["Growth factor"]}`)
   growText.rightAlignText()
   growText.textColor = Color.white()
   growText.textOpacity = 0.8
@@ -244,20 +174,8 @@ function createWidget(data) {
   growText.minimumScaleFactor = 0.5
 
   widget.addSpacer()
-  
-  let bar = vaccGraph(data["vacpc"][0], data["vacpc"][1], data["vacpc"][2], 232, 3).getImage()
-  widget.addImage(bar)
-  
-  let vaxText = widget.addText(`${data["stats"]["Third Dose 18+"]} boosted`)
-  vaxText.rightAlignText()
-  vaxText.textColor = Color.white()
-  vaxText.textOpacity = 0.8
-  vaxText.font = Font.regularSystemFont(10)
-  vaxText.minimumScaleFactor = 0.5
 
-  widget.addSpacer()
-
-  Object.entries(data["widget"]).forEach(([key, value]) => {
+  Object.entries(data.widget).forEach(([key, value]) => {
     let stack = widget.addStack()
     stack.spacing = 5
     let labelText = stack.addText(key)
@@ -277,25 +195,25 @@ function createWidget(data) {
 function createTable(data) {
   let table = new UITable()
   let dark = Device.isUsingDarkAppearance()
-  let growth = data["growth"]
+  let grow = data.stats["Growth factor"] < 1
   let bgColor =
-    growth < 1 && dark ? new Color("003300") :
+    grow && dark ? new Color("003300") :
     dark ? new Color("330000") :
-    growth < 1 ? new Color("b8ffb1") :
+    grow ? new Color("b8ffb1") :
     new Color("ffb1b1")
   let fgColor = dark ? "ffffff" : "000000"
   let head = new UITableRow()
   head.isHeader = true
   head.addText('COVID-19 Statistics')
-  head.addText(data['date']).rightAligned()
+  head.addText(data.date).rightAligned()
   head.backgroundColor = bgColor
   table.addRow(head)
   let row = new UITableRow()
   row.backgroundColor = bgColor
-  let image = caseGraph(data["national"], data["state"], 640, 50, fgColor).getImage()
+  let image = caseGraph(data.national, data.state, 640, 50, fgColor).getImage()
   row.addImage(image)
   table.addRow(row)
-  Object.entries(data["stats"]).forEach(([key, value]) => {
+  Object.entries(data.stats).forEach(([key, value]) => {
     let row = new UITableRow()
     row.addText(key)
     row.addText(value).rightAligned()
@@ -305,23 +223,14 @@ function createTable(data) {
   table.present()
 }
 
-function display(data) {
-  if (config.runsInWidget) {
-    let widget = createWidget(data)
-    Script.setWidget(widget)
-  } else {
-    createTable(data)
-  }
-  Script.complete()
+if (config.runsInWidget) {
+  let data = loadData()
+  let widget = createWidget(data)
+  Script.setWidget(widget)
+} else {
+  let data = await getData(url)
+  data = saveData(data)
+  createTable(data)
 }
 
-let updated = await checkData()
-let data = await loadData()
-if (updated || false) { // set true if developing
-  if (data["updated"] != updated) {
-    data = await saveData(updated)
-    display(data)
-  } else {
-    display(data)
-  }
-}
+Script.complete()
